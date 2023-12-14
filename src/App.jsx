f5239922a3dbf1e04cccc23c4773c0fd2976cf24
khalad007 <www.arifhossain49919@gmail.com>
2023-12-14 15:11:14 +0600

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainContainer from "./Component/MainContainer"
+
+function App() {
+  return (
+    <div className="bg-base-300 min-h-screen">
+      app
+      <MainContainer></MainContainer>
+    </div>
+  )
+}
+
+export default App
